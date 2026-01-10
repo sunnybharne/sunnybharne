8aeb0397eff4c7d49479f3e207dd1f6e7416742f
sunny bharne <sunny.bharne@outlook.com>
2026-01-10 11:28:03 +0200

    added: js task

---
+++ task.js
@@ -0,0 +1,27 @@
+// FIXME: work
+// Checklist for sub move.
+// Update the policy pipeline stages.
+// Apply for the new pr.
+// Update the policy deletion logic.
+// Change the phone plans to do stuff.
+//
+// FIXME: non work
+// move money from india to finland.
+// Check the fira stuff.
+// Download the docs.respond if you have to.
+// Fix the github stuff.
+// Fix the azure devops connections. and check if eveything works fine.
+// Walking workout.
+// Driving license stuff.
+// Read the contract.
+// Read the Fira Contract.
+//
+// FIXME: youtube stuff
+// think about your first vedio on youtube. create a storyboarding plan in the readme file.
+// Make the bigger picture storybaord.
+// Plan the shorter picture storyboard.
+// Platn the very shorter term storyboard.
+// Think of the single cuts that for that shorter term storyboarding section.
+//
+// FIXME: Job Stuff (Plan the job stuff)
+// FIXME: Create social media plan
