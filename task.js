a51c6b03a4c44f2925f8dddc097d4592042e32fe
sunny bharne <sunny.bharne@outlook.com>
2026-01-10 21:18:17 +0200

    fixed banner

--- task.js
+++ task.js
@@ -4,15 +4,11 @@
 // Update the policy deletion logic.
 // Change the phone plans to do stuff.
 //
-// FIXME: non work
-// Fix the azure devops connections. and check if eveything works fine.
-//
 // FIXME: youtube stuff
-//
 // think about your first vedio on youtube. create a storyboarding plan in the readme file.
 // Make the bigger picture storybaord.
 // Plan the shorter picture storyboard.
-// Platn the very shorter term storyboard.
+// Plan the very shorter term storyboard.
 // Think of the single cuts that for that shorter term storyboarding section.
 //
 // FIXME: Job Stuff (Plan the job stuff)
